49e856f98596baa9a88e91fbbf016bd7f8b5a14b
Momo Kornher <kornherm@amazon.co.uk>
2025-08-08 12:45:01 +0200

    feat: test trusted publishers

--- .projenrc.ts
+++ .projenrc.ts
@@ -12,11 +12,9 @@ const project = new cdk.JsiiProject({
   release: true,
   repositoryUrl: "https://github.com/projen/canary-testing.git",
   prettier: true,
-  jsiiVersion: "5.4.x",
-  typescriptVersion: "5.4.x",
-  jestOptions: {
-    jestVersion: "^29",
-  },
+  jsiiVersion: "5.9.x",
+  typescriptVersion: "5.9.x",
+  workflowNodeVersion: "24.x",
   autoApproveUpgrades: true,
   autoApproveOptions: {
     allowedUsernames: ["cdklabs-automation", "dependabot[bot]"],
@@ -32,35 +30,21 @@ const project = new cdk.JsiiProject({
   peerDependencyOptions: {
     pinnedDevDependency: false,
   },
+  releaseEnvironment: "npm",
   npmAccess: NpmAccess.PUBLIC,
-  publishToMaven: {
-    javaPackage: "io.github.cdklabs.projen_canary_package",
-    mavenGroupId: "io.github.cdklabs",
-    mavenArtifactId: "projen_canary_package",
-    mavenEndpoint: "https://s01.oss.sonatype.org",
+  publishToPypi: {
+    trustedPublishing: true,
+    distName: "projen.canary-package",
+    module: "projen.canary_package",
+    githubEnvironment: "release",
   },
+  // publishToMaven: {
+  //   javaPackage: "io.github.cdklabs.projen_canary_package",
+  //   mavenGroupId: "io.github.cdklabs",
+  //   mavenArtifactId: "projen_canary_package",
+  //   mavenEndpoint: "https://s01.oss.sonatype.org",
+  // },
 } satisfies JsiiProjectOptions);
-
-const label = "backport-to-";
-const cond =
-  "github.event.pull_request.merged && !(contains(github.event.pull_request.labels.*.name, 'backport'))";
-project.github?.tryFindWorkflow("backport")?.file?.patch(
-  JsonPatch.add("/jobs/backport/if", cond),
-  JsonPatch.add("/jobs/backport/steps/0", {
-    id: "check_labels",
-    name: "Check for backport labels",
-    run: [
-      "labels='${{ toJSON(github.event.pull_request.labels.*.name) }}'",
-      `matched=$(echo $labels | jq '.|map(select(startswith("${label}"))) | length')`,
-      'echo "matched=$matched"',
-      'echo "matched=$matched" >> $GITHUB_OUTPUT',
-    ].join("\n"),
-  }),
-  JsonPatch.add(
-    "/jobs/backport/steps/1/if",
-    "fromJSON(steps.check_labels.outputs.matched) > 0",
-  ),
-);
 
 new MergeQueue(project, {
   mergeBranch: "main",
@@ -69,20 +53,25 @@ new MergeQueue(project, {
   },
 });
 
+// remove npm token
+project.github
+  ?.tryFindWorkflow("release")
+  ?.file?.patch(JsonPatch.remove("/jobs/release_npm/steps/9/env/NPM_TOKEN"));
+
 // fix java version
-const javaVersion = "11";
-project.github?.tryFindWorkflow("build")?.file?.patch(
-  JsonPatch.replace("/jobs/package-java/steps/0/with", {
-    distribution: "corretto",
-    "java-version": javaVersion,
-  }),
-);
-project.github?.tryFindWorkflow("release")?.file?.patch(
-  JsonPatch.replace("/jobs/release_maven/steps/0/with", {
-    distribution: "corretto",
-    "java-version": javaVersion,
-  }),
-);
+// const javaVersion = "11";
+// project.github?.tryFindWorkflow("build")?.file?.patch(
+//   JsonPatch.replace("/jobs/package-java/steps/0/with", {
+//     distribution: "corretto",
+//     "java-version": javaVersion,
+//   }),
+// );
+// project.github?.tryFindWorkflow("release")?.file?.patch(
+//   JsonPatch.replace("/jobs/release_maven/steps/0/with", {
+//     distribution: "corretto",
+//     "java-version": javaVersion,
+//   }),
+// );
 
 project.gitignore?.addPatterns("!vendor/*");
 
